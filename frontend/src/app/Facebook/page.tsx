e644d7bf47af95385d572da9025f9578a8d3c55a
Nguyen-Trung-Tien <2251120447@ut.edu.vn>
2025-10-16 17:12:02 +0700

    learn next

---
+++ frontend/src/app/Facebook/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import Button from "react-bootstrap/Button";
+
+const page = () => {
+  const router = useRouter();
+  const handleBackHome = () => {
+    router.push("/");
+  };
+  return (
+    <div>
+      page
+      <div>
+        <Button as="a" variant="success" onClick={() => handleBackHome()}>
+          Back Home
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default page;
